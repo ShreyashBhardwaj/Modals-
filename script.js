d05b039ee1884e3d61a4be5b928b28c00668cb01
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-10-03 22:46:34 +0530

    Modals Done

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const closeModalButton = document.querySelector(".close-modal");
+const allModalButton = document.querySelectorAll(".show-modal");
+
+allModalButton.forEach((element) => {
+  element.addEventListener("click", showModal);
+});
+
+closeModalButton.addEventListener("click", hideModal);
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    hideModal();
+  }
+});
+
+function showModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function hideModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
